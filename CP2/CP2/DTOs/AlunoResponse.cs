15f0b9437c8b609f16d27a4ffa5288f61658d8ed
Carlos Clementino <carlossantiagoclementino36@gmail.com>
2025-10-31 15:58:17 -0300

    Remove Id property from AlunoResponse

--- CP2/CP2/DTOs/AlunoResponse.cs
+++ CP2/CP2/DTOs/AlunoResponse.cs
@@ -7,13 +7,11 @@ public class AlunoResponse
     public string Nome { get; set; }
     public string Rm { get; set; }
     
-    public Guid Id { get; set; }
     
     public static AlunoResponse ToResponse(Aluno aluno)
     {
         return new AlunoResponse
         {
-            Id = aluno.Id,
             Nome = aluno.Nome,
             Rm = aluno.Rm
         };
