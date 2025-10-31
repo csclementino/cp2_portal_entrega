c48831c5aa5809a93801ef0777f7aa28835b2eca
Carlos Clementino <carlossantiagoclementino36@gmail.com>
2025-10-31 15:58:36 -0300

    Update ProfessorResponse.cs

--- CP2/CP2/DTOs/ProfessorResponse.cs
+++ CP2/CP2/DTOs/ProfessorResponse.cs
@@ -7,14 +7,12 @@ public class ProfessorResponse
     public string Nome { get; set; }
     public string Pf { get; set; }
     
-    public Guid Id { get; set; }
     public static ProfessorResponse ToResponse(Professor professor)
     {
         return new ProfessorResponse
         {
             Nome = professor.Nome,
-            Pf = professor.Pf,
-            Id = professor.Id,
+            Pf = professor.Pf
         };
     }
 }
